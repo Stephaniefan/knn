9dde2048c4540dde7afe8f6c825e740700207524
Stephaniefan <ningxinfan0903@gmail.com>
2015-03-29 16:57:58 -0400

    try again

--- src/knn/test.java
+++ src/knn/test.java
@@ -2,7 +2,8 @@ package knn;
 
 public class test {
 	public static void main(String[] args) {
+
 		System.out.println("hello world");
-		//System.out.println("!!!");
+		System.out.println("hahahah");
 	}
 }
