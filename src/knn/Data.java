8374a010bb870499cd58cda97fac93bdbf9b9c53
daviddai9112 <daviddai9112@gmail.com>
2015-03-31 14:53:33 -0400

    Data processing

---
+++ src/knn/Data.java
@@ -0,0 +1,45 @@
+package knn;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Data class contains attributes for an entry/record
+ * It stores data in a HashMap:
+ * */
+
+public class Data {
+    // key: attribute name
+    // value: attribute value, stored in string
+    private HashMap<String, String> data;
+
+    public HashMap<String, String> getData() {
+        return data;
+    }
+    
+    public String getData(String key) {
+        return data.get(key);
+    }
+    
+    public Data (ArrayList<String> attributeList, String content) {
+        data = new HashMap<String, String>();
+        String[] values = content.split(",");
+        for (int i = 0; i < attributeList.size(); ++i) {
+            data.put(attributeList.get(i), values[i]);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Data [data=" + data + "]";
+    }
+
+    public static void main(String[] args) {
+        @SuppressWarnings("serial")
+        ArrayList<String> attributeList = new ArrayList<String> ()
+                {{add("A"); add("B"); add("C");}};
+        String content = "Fund,Student,0.64";
+        Data d = new Data(attributeList, content);
+        System.out.println(d);
+    }
+
+}
---
+++ src/knn/DataSet.java
@@ -0,0 +1,133 @@
+package knn;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class DataSet {
+    private HashMap<String, Attribute> attributeMap;    // attri name -> attribute info. 
+    private ArrayList<String> attributeList;                        // list of attributes
+    private ArrayList<Data> dataList;                                        // list of all data
+    private String relation;
+    private String objective;
+    
+    public HashMap<String, Attribute> getAttributeMap() { return attributeMap; }
+    public ArrayList<String> getAttributeList() { return attributeList; }
+    public ArrayList<Data> getData() { return dataList; }
+    public String getRelation() { return relation; }
+    public String getObjective() { return objective; }
+    
+    public void setObjective(String v) {  objective = v; }
+    
+    public DataSet() {
+        attributeMap = new HashMap<String, Attribute>();
+        attributeList = new ArrayList<String>();
+        dataList = new ArrayList<Data>();
+    }
+    
+    public int size() {
+    	return dataList.size();
+    }
+    
+    public DataSet(String rel, HashMap<String, Attribute> map, ArrayList<String> attriList, String obj) {
+    	relation = rel;
+        attributeMap = map;
+        attributeList = attriList;
+        dataList = new ArrayList<Data>();
+        objective = obj;
+    }
+
+    /**
+     * @param String attribute: name of attribute
+     * @param String value: decision point, for class, it represents a category; for real number, it halves the range
+     * @param boolean left: only used in real number, represents <= value OR > value
+     * 
+     * @return DataSet: the targeted sub dataset
+     * */
+    public DataSet getSubSet (String attribute, String value, boolean left, ArrayList<String> attriList) {
+    	DataSet subset = new DataSet(this.relation, this.attributeMap, attriList, this.getObjective());
+    	if (attributeMap.get(attribute).isRealNum()) {
+    		for (Data data : dataList) {
+    			if (left && Double.parseDouble(data.getData(attribute)) <= Double.parseDouble(value)) {
+    				subset.add(data);
+    			}
+    			else if (!left && Double.parseDouble(data.getData(attribute)) > Double.parseDouble(value)) {
+    				subset.add(data);
+    			}
+    		}
+    	} else {
+    		for (Data data : dataList) {
+    			if (data.getData(attribute).equals(value)) {
+    				subset.add(data);
+    			}
+    		}
+    	}
+    	return subset;
+    }
+    
+    public void add (Data d) {
+    	dataList.add(d);
+    }
+    
+    public boolean isAttriReal (String attri) {
+    	return attributeMap.get(attri).isRealNum();
+    }
+
+    public void readDataFromFile(String fileName) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String buffer = null;
+            while ((buffer = reader.readLine()) != null && !buffer.matches("@data")) {
+                if (!buffer.isEmpty()) {
+                    if (buffer.startsWith("@relation")) {
+                        relation = buffer.split(" ")[1];
+                    } else if (buffer.startsWith("@attribute")) {
+                        String[] attr = buffer.split(" ");
+                        attributeList.add(attr[1]);
+                        attributeMap.put(attr[1], new Attribute(attr[1], attr[2]));
+                    }
+                }
+            }
+            while ((buffer = reader.readLine()) != null) {
+                dataList.add(new Data(attributeList, buffer));
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {    reader.close();} 
+                catch (IOException e1) {}
+            }
+        }
+        
+        this.objective = attributeList.get(attributeList.size() - 1);
+        attributeList.remove(this.objective);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\nRelation : " + relation + "\n");
+        for (String key : attributeMap.keySet()) {
+            sb.append(attributeMap.get(key) + "\n");
+        }
+        for (Data dt : dataList) {
+            sb.append(dt + "\n");
+        }
+        return "DataSet [" +sb.toString() + "]";
+    }
+
+    public static void main(String[] args) {
+        DataSet data = new DataSet();
+        data.readDataFromFile("trainProdSelection.arff");
+        System.out.println(data);
+        //System.out.println(data.getSubSet("eCredit", "5", true, data.getAttributeList()));
+    }
+
+}
